12f798c9052af9f8ebf3b91a4a08759e6aafaefe
AlisonAO-RV <alisonrv@msn.com>
2020-10-07 12:48:34 -0300

    Novos Arquivos

---
+++ 1_INICIANTE/1001_04_ExtremamenteBasico.cpp
@@ -0,0 +1,23 @@
+/* Criado Por: Alison Alain de Oliveira
+ * Estudante de Engenharia de Software
+ * Universidade: UNIRV - Rio Verde - GO
+ * Ano: 2020
+ * 
+ * Problema encontrado em: https://www.urionlinejudge.com.br/judge/pt/problems/view/1001
+ * Nivel: 4
+ */ 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, x;
+    
+    cin >> a >> b;
+    x = a + b;
+    cout << "X = " << x <<"\n";
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Treinamento Maratona de Programação
+
+Repositorio com opção de soluções dos problemas encontrado em https://www.urionlinejudge.com.br/judge/pt/categories
+
+Linguagem Utilizada C++
+
+essas Soluções são feitas pelo Alison Alain de Oliveira (Cursando Faculdade de Engenharia de Software - UNIRV - 2020), como se trata de estudo e treinamento, pode existir soluções mais eficientes.
+
+![AlisonAO-RV](./imgs/Img_AlisonAO-RV.jpg)
